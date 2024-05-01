9f6eea4954c160994984f2f9afc29adfd11cf663
alireza19330 <alireza19330@gmail.com>
2024-05-01 10:21:27 +1000

    add unit test config

---
+++ src/test/java/com/vg/application/config/SpringTestConfig.java
@@ -0,0 +1,11 @@
+package com.vg.application.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.vg")
+public class SpringTestConfig {
+}
