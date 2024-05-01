211462185dc1de99289baf0cd2a64e7aadb8a69d
alireza19330 <alireza19330@gmail.com>
2024-05-01 10:21:10 +1000

    add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into consumers values (1, 'api-key-1');
+insert into consumers values (2, 'api-key-2');
+insert into consumers values (3, 'api-key-3');
+insert into consumers values (4, 'api-key-4');
+insert into consumers values (5, 'api-key-5');
